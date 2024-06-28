9e762c200fedb965e58deaa88f5fb13cdd5a5b73
William Brendel <brendel.william@gmail.com>
2024-06-28 11:12:19 -0700

    getscriptnode

--- src/scripts/initHead.mjs
+++ src/scripts/initHead.mjs
@@ -8,24 +8,24 @@ import {
 } from 'https://crossroads-lab.github.io/Client/src/index.mjs';
 
 // Add title to head.
-addTitle(COMPANY_INFORMATION.NAME);
+addTitle({title: COMPANY_INFORMATION.NAME, comment: 'Document title'});
 
 // Add default meta tags, i.e. charset and viewport.
-addMetaTag({charset: 'utf-8', comment: 'DEFAULT META TAGS'});
+addMetaTag({charset: 'utf-8', comment: 'Default meta tags'});
 addMetaTag({name: 'viewport', content: 'width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover, user-scalable=no'});
 
 // Add SEO | Open Graph meta tags.
-COMPANY_INFORMATION.NAME && addMetaTag({property: 'og:title', content: COMPANY_INFORMATION.NAME});
+COMPANY_INFORMATION.NAME && addMetaTag({property: 'og:title', content: COMPANY_INFORMATION.NAME, comment: 'SEO | Open Graph meta tags'});
 COMPANY_INFORMATION.DESCRIPTION && addMetaTag({property: 'og:description', content: COMPANY_INFORMATION.DESCRIPTION});
 const image =  COMPANY_INFORMATION.LOGO || COMPANY_INFORMATION.IMAGE;
 image && addMetaTag({property: 'og:image', content: image});
 
 // Add css.
 const css = LINKS.CSS || LINKS.STYLES;
-css && addLink({href: css, rel: 'stylesheet', type: 'text/css'});
+css && addLink({href: css, rel: 'stylesheet', type: 'text/css', comment: 'Styles'});
 
 // Add favicon.
 const favicon = LINKS.FAVICON || LINKS.ICON;
-favicon && addLink({href: favicon, rel: 'shortcut icon', type: 'image/x-icon'});
+favicon && addLink({href: favicon, rel: 'shortcut icon', type: 'image/x-icon', comment: 'Favicon'});
 
 removeScriptNode(import.meta.url);
