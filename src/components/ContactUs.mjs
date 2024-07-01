35f7c3f77573fb34d05a44a786e5cf212425e4d6
William Brendel <brendel.william@gmail.com>
2024-07-01 16:33:19 -0700

    contact us

--- src/components/ContactUs.mjs
+++ src/components/ContactUs.mjs
@@ -38,7 +38,7 @@ const createTemplate = () => {
     background: var(--blueberry);
     padding: var(--padding) var(--padding-width-90-percent);
   }
-  :host img {
+  img {
     filter: saturate(1) brightness(1);
   }
   `;
@@ -53,6 +53,7 @@ const createTemplate = () => {
   // Logo.
   logoLink.classList.add('link');
   logoLink.classList.add('cursor-n-resize');
+  logoLink.classList.add('icon');
   logoLink.setAttribute('href', '#front-page-slide-show');
   logoLink.setAttribute('title', 'Go to top of the page');
   logoImg.setAttribute('src', LOGO);
@@ -61,6 +62,7 @@ const createTemplate = () => {
   // Description.
   description.setAttribute('title', description.innerHTML = DESCRIPTION);
   description.classList.add('font-size-12-14');
+  description.classList.add('text-align-center');
 
   // Socials.
   socials.classList.add('socials');
@@ -75,10 +77,8 @@ const createTemplate = () => {
     link.setAttribute('title', `Go to our ${social.value || social.name || social.title || 'social'} page`);
     link.setAttribute('target', '_blank');
     link.setAttribute('href', social.url || social.href || social.link);
-    link.classList.add('button');
-    link.classList.add('icon');
-    link.classList.add('image-container');
-    link.classList.add('contain');
+    link.classList.add('link');
+    img.classList.add('icon');
     img.setAttribute('src', social.src, social.img || social.icon);
     img.setAttribute('alt', `${social.value || social.name || social.title || 'Social'} logo`);
   }
