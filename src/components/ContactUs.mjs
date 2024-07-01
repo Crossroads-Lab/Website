06f853dc3c25137d6aa58c6cebc307defa0d5252
William Brendel <brendel.william@gmail.com>
2024-07-01 16:21:03 -0700

    contact us

---
+++ src/components/ContactUs.mjs
@@ -0,0 +1,101 @@
+import {
+  CREATION_YEAR,
+  DESCRIPTION,
+  LOGO,
+  SOCIALS
+} from ('../data/companyInformation.mjs');
+
+// Extend generic HTMLElement interface.
+export class ContactUs extends HTMLElement {
+  // Constructor.
+  constructor() {
+    super();
+
+    // Create template if needed.
+    template || (template = createTemplate());
+
+    // Attach shadow DOM to element.
+    const shadow = this.attachShadow({mode: 'open'});
+    shadow.appendChild(template.cloneNode(true));
+  }
+}
+
+// Template.
+let template;
+const createTemplate = () => {
+  const template = document.createDocumentFragment();
+
+  // Style.
+  template.appendChild(document.createElement('style')).innerHTML = `
+  :host {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: var(--gap);
+    color: var(--light);
+    background: var(--blueberry);
+    padding: var(--padding) var(--padding-width-90-percent);
+  }
+  :host img {
+    filter: saturate(1) brightness(1);
+  }
+  `;
+
+  // Content.
+  const logoLink = template.appendChild(document.createElement('a')),
+    logoImg = logoLink.appendChild(document.createElement('img')),
+    description = template.appendChild(document.createElement('div')),
+    socials = template.appendChild(document.createElement('div')),
+    copyright = template.appendChild(document.createElement('div'));
+
+  // Logo.
+  logoLink.classList.add('link');
+  logoLink.classList.add('cursor-n-resize');
+  logoLink.setAttribute('href', '#front-page-slide-show');
+  logoLink.setAttribute('title', 'Go to top of the page');
+  logoImg.setAttribute('src', LOGO);
+  logoImg.classList.add('icon');
+
+  // Description.
+  description.setAttribute('title', description.innerHTML = DESCRIPTION);
+  description.classList.add('font-size-12-14');
+
+  // Socials.
+  socials.classList.add('socials');
+  socials.classList.add('row');
+  socials.classList.add('middle');
+  socials.classList.add('center');
+  socials.classList.add('gap-half');
+  for (let i = 0, s = SOCIALS || [], l = s.length, link, img, social; i !== l; ++i) {
+    link = socials.appendChild(document.createElement('a'));
+    img = link.appendChild(document.createElement('img'));
+    social = s[i];
+    link.setAttribute('title', `Go to our ${social.value || social.name || social.title || 'social'} page`);
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', social.url || social.href || social.link);
+    link.classList.add('button');
+    link.classList.add('icon');
+    link.classList.add('image-container');
+    link.classList.add('contain');
+    img.setAttribute('src', social.src, social.img || social.icon);
+    img.setAttribute('alt', `${social.value || social.name || social.title || 'Social'} logo`);
+  }
+
+  // Copyright.
+  copyright.classList.add('text-align-center');
+  copyright.classList.add('text-wrap-balance');
+  copyright.classList.add('font-size-12-14');
+  const year = new Date(Date.now()).getFullYear();
+  copyright.setAttribute('title', copyright.innerHTML = `Copyright © ${CREATION_YEAR && CREATION_YEAR != year && `${CREATION_YEAR}-`}${year} all rights reserved`);
+
+  // Output.
+  return template;
+}
+
+// Default export.
+export default ContactUs;
+
+// Register component.
+customElements.define('contact-us', ContactUs);
--- src/data/companyInformation.mjs
+++ src/data/companyInformation.mjs
@@ -3,6 +3,24 @@ export const NAME = COMPANY_INFORMATION.NAME = 'Crossroads Venture Studio';
 export const DESCRIPTION = COMPANY_INFORMATION.DESCRIPTION = 'CROSSROADS Studio is an in-house startup incubator focused on projects with high return on investment, with minimum development and launch time and maximum impact';
 export const MEETING_LINK = COMPANY_INFORMATION.MEETING_LINK = 'https://calendly.com/william-brendel/30min';
 export const EMAIL = COMPANY_INFORMATION.EMAIL = 'crossroadsventurestudio@gmail.com';
+export const SOCIALS = COMPANY_INFORMATION.SOCIALS = [
+  {
+    value: 'LinkedIn',
+    href: 'https://www.linkedin.com/company/crossroads-venture-studio',
+    src: 'https://crossroads-lab.github.io/Design-System/icons/socials/linkedin-light.svg'
+  },
+  {
+    value: 'Instagram',
+    href: '',
+    src: 'https://crossroads-lab.github.io/Design-System/icons/socials/instagram-light.svg'
+  },
+  {
+    value: 'x',
+    href: '',
+    src: 'https://crossroads-lab.github.io/Design-System/icons/socials/x-light.svg'
+  }
+];
+export const CREATION_YEAR = COMPANY_INFORMATION.CREATION_YEAR = 2023;
 export const FAVICON = COMPANY_INFORMATION.FAVICON = 'assets/favicon.ico';
 export const STYLES = COMPANY_INFORMATION.STYLES = 'src/css/styles.css';
 
