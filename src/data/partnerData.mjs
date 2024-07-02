9b4f06d2a43cab3227bd0f7a770107df2f7cbad4
William Brendel <brendel.william@gmail.com>
2024-07-02 13:53:45 -0700

    partners

--- src/data/partnerData.mjs
+++ src/data/partnerData.mjs
@@ -2,47 +2,56 @@ export const PARTNERS = [
   {
     src: 'assets/partners/angelytics.svg',
     name: 'Angelytics',
-    href: 'https://www.angelytics.ai/'
+    href: 'https://www.angelytics.ai/',
+    description: 'Makes your analytics HIPAA compliant'
   },
   {
     src: 'assets/partners/automarketer.svg',
     name: 'Automarketer',
-    href: 'https://www.automarketer.ai/'
+    href: 'https://www.automarketer.ai/',
+    description: 'Your AI social marketing manager'
   },
   {
     src: 'assets/partners/gococo.svg',
     name: 'Gococo',
-    href: 'https://www.gococo.ai/'
+    href: 'https://www.gococo.ai/',
+    description: 'AI Art Generation + Print-on-Demand made easy'
   },
-  {
-    src: 'assets/partners/fuelbetter.svg',
-    name: 'Fuelbetter',
-    href: 'https://www.fuelbetter.co/'
-  },
+  // {
+  //   src: 'assets/partners/fuelbetter.svg',
+  //   name: 'Fuelbetter',
+  //   href: 'https://www.fuelbetter.co/',
+  //   description: ''
+  // },
   {
     src: 'assets/partners/saucy.svg',
     name: 'Saucy',
-    href: 'https://www.getsaucy.ai/'
+    href: 'https://www.getsaucy.ai/',
+    description: 'An AI-based recipe reverse engineering platform'
   },
   {
     src: 'assets/partners/ramset.svg',
     name: 'Ramset',
-    // href: 'https://www.ramset.com/'
+    // href: 'https://www.ramset.com/',
+    description: 'A CRP software for steel suppliers and manufacturers'
   },
   {
     src: 'assets/partners/fildariane.svg',
     name: 'Fil d\'Ariane',
-    href: 'https://www.thefildariane.com/'
+    href: 'https://www.thefildariane.com/',
+    description: 'An consulting agency for french startups targeting the US market'
   },
   {
     src: 'assets/partners/second-chance.svg',
     name: 'Second chance',
+    description: 'A HRM for prison'
     // href: 'https://www.second-chance.health/'
   },
   {
     src: 'assets/partners/planet-pulse.svg',
     name: 'Planet Pulse',
-    href: 'https://www.planet-pulse.fr/'
+    href: 'https://www.planet-pulse.fr/',
+    description: 'A social media for the planet'
   }
 ];
 
