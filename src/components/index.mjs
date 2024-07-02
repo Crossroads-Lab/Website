ef423db886ebc309974b9768a1d4c04ef66c9051
William Brendel <brendel.william@gmail.com>
2024-07-02 15:27:07 -0700

    partners

--- src/components/index.mjs
+++ src/components/index.mjs
@@ -2,4 +2,5 @@ export { default as FooterInfo } from './FooterInfo.mjs';
 export { default as FrontPage } from './FrontPage.mjs';
 export { default as FrontPageSlideShow } from './FrontPageSlideShow.mjs';
 export { default as PartnerList } from './PartnerList.mjs';
+export { default as SplashScreen } from './SplashScreen.mjs';
 export { default as TopNavbar } from './TopNavbar.mjs';
