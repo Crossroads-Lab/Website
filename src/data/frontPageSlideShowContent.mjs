d807f2f4aec1e9e291de87563407f8629bf6e3a7
William Brendel <brendel.william@gmail.com>
2024-07-02 12:50:08 -0700

    partners

--- src/data/frontPageSlideShowContent.mjs
+++ src/data/frontPageSlideShowContent.mjs
@@ -4,7 +4,8 @@ export const FRONT_PAGE_SLIDE_SHOW_CONTENT = [
     description: 'The venture studio that transform your ideas into reality',
     link: 'Co-found with us',
     background: 'background-0.png',
-    animation: 'zoom-out-left'
+    animation: 'zoom-out-left',
+    href: '#contact-us'
   },
   {
     headline: 'Your project, our execution',
@@ -12,42 +13,48 @@ export const FRONT_PAGE_SLIDE_SHOW_CONTENT = [
     link: 'Co-found with us',
     background: 'background-1.jpg',
     position: 'top left',
-    animation: 'zoom-out'
+    animation: 'zoom-out',
+    href: '#contact-us'
   },
   {
     headline: 'The venture studio that has your back',
     description: 'From concept to launch, we\'re here to support you',
     link: 'Co-found with us',
     background: 'background-2.jpg',
-    animation: 'zoom-out-right'
+    animation: 'zoom-out-right',
+    href: '#contact-us'
   },
   {
     headline: 'Learn fast, succeed faster',
     description: 'Leverage our team of tech, product, design and marketing experts',
     link: 'Co-found with us',
     background: 'background-3.jpg',
-    animation: 'zoom-out-top'
+    animation: 'zoom-out-top',
+    href: '#contact-us'
   },
   {
     headline: 'From idea to market',
     description: 'The easiest and fastest way to create a new AI tech venture',
     link: 'Co-found with us',
     background: 'background-4.png',
-    animation: 'zoom-out'
+    animation: 'zoom-out',
+    href: '#contact-us'
   },
   {
     headline: 'Are you ready?',
     description: 'Let\'s innovate and build the products of tomorrow together',
     link: 'Co-found with us',
     background: 'background-5.jpg',
-    animation: 'zoom-out-left'
+    animation: 'zoom-out-left',
+    href: '#contact-us'
   },
   {
     headline: 'Join the mouvement',
     description: 'Time for you to be part of the next generation of entrepreneurs',
     link: 'Co-found with us',
     background: 'background-6.jpg',
-    animation: 'zoom-out-top'
+    animation: 'zoom-out-top',
+    href: '#contact-us'
   }
 ];
 
