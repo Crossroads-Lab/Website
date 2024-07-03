c42f8b7beb0a5237edea5146dd58ab5781f25763
William Brendel <brendel.william@gmail.com>
2024-07-03 14:55:09 -0700

    refactor

--- src/data/links.mjs
+++ src/data/links.mjs
@@ -1,6 +1,6 @@
 export const LINKS = {};
 export const FAVICON = LINKS.FAVICON = 'assets/favicon.ico';
-export const STYLES = LINKS.STYLES = 'src/css/styles.css';
+export const STYLES = LINKS.STYLES = 'src/css/index.css';
 
 // Default export.
 export default LINKS;
