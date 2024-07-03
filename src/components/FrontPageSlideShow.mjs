95359b0194ba25da14a9ac5214d477141b2ce176
William Brendel <brendel.william@gmail.com>
2024-07-03 16:46:58 -0700

    opacity

--- src/components/FrontPageSlideShow.mjs
+++ src/components/FrontPageSlideShow.mjs
@@ -3,63 +3,42 @@ export class FrontPageSlideShow extends HTMLElement {
   // Constructor.
   constructor() {
     super();
+  }
 
-    // Create template if needed.
-    template || (template = createTemplate());
-
-    // Attach shadow DOM to element.
-    const shadow = this.attachShadow({mode: 'open'});
-    shadow.appendChild(template.cloneNode(true));
-
-    // Get slotted content and create animation.
-    const children = shadow.childNodes[1].assignedElements(),
-      [t, u = 'ms'] = (this.getAttribute('ms') || this.getAttribute('time') || '10000').split(/(ms|s)/),
-      ms = parseFloat(t) * (u.trim().toLowerCase() === 's' && 1000 || 1),
-      l = children.length;
+  connectedCallback() {
+    // Create template if needed, and get children.
+    const children = this.appendChild(
+      (template || (template = createTemplate())).cloneNode(true)
+    ).assignedElements() || [],
+    [t, u = 'ms'] = (this.getAttribute('ms') || this.getAttribute('time') || '10000').split(/(ms|s)/),
+    ms = parseFloat(t) * (u.trim().toLowerCase() === 's' && 1000 || 1),
+    l = children.length;
     let index = (this.getAttribute('startIndex') || Math.floor(Math.random() * l)) % l;
+
+    // Create animation.
     for (let i = 0, c; i !== l; ++i) {
       c = children[i];
-      c.style.position = 'absolute';
-      c.style.top = c.style.left = 0;
-      i === index && (
-        c.startAnimation(),
-        c.style.transition = null,
-        c.style.opacity = 1
-      ) || (c.style.opacity = 0);
+      c.classList.add('position-absolute');
+      i === index && c.startAnimation() || c.classList.add('opacity-0-percent');
     }
 
     // Rotate slides.
     setInterval(() => {
       const cur = children[index],
         next = children[index = (index + 1) % l];
-      cur.style.transition = next.style.transition = 'opacity 3s';
-      cur.style.opacity = 0;
+      cur.classList.add('opacity-0-percent');
+      next.classList.remove('opacity-0-percent');
       setTimeout(() => cur.cancelAnimation(), (~~ms) >> 1);
-      next.style.opacity = 1;
       next.startAnimation();
-      setTimeout(() => cur.style.transition = next.style.transition = null, 3100);
     }, ms);
+
   }
 }
 
 // Template.
 let template;
 const createTemplate = () => {
-  const template = document.createDocumentFragment();
-
-  // Style.
-  template.appendChild(document.createElement('style')).innerHTML = `
-  :host {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    display: block;
-    background: black;
-  }
-  `;
-
-  // Content.
-  template.appendChild(document.createElement('slot'));
+  const template = document.createElement('slot');
 
   // Output.
   return template;
