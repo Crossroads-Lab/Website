a49a5f3db94ab6ccd200eaae1669ccbd2e309a64
William Brendel <brendel.william@gmail.com>
2024-07-03 14:52:23 -0700

    refactor

--- src/data/topNavbarContent.mjs
+++ src/data/topNavbarContent.mjs
@@ -1,9 +1,9 @@
 // Imports.
-import { LOGO, NAME } from './companyInformation.mjs';
+import { LOGO as _LOGO, NAME as _NAME } from './companyInformation.mjs';
 
 export const TOP_NAVBAR_CONTENT = {};
-export const LOGO = TOP_NAVBAR_CONTENT.LOGO = LOGO;
-export const NAME = TOP_NAVBAR_CONTENT.NAME = NAME;
+export const LOGO = TOP_NAVBAR_CONTENT.LOGO = _LOGO;
+export const NAME = TOP_NAVBAR_CONTENT.NAME = _NAME;
 export const TITLE = TOP_NAVBAR_CONTENT.TITLE = `Welcome to ${NAME}`;
 
 // Default export.
